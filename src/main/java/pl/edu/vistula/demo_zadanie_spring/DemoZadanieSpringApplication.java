13692f840baf24e929867b52f1909919081f5c52
jgerc <jacekge3@gmail.com>
2024-12-29 20:51:27 +0100

    commit

---
+++ src/main/java/pl/edu/vistula/demo_zadanie_spring/DemoZadanieSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.demo_zadanie_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZadanieSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoZadanieSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/demo_zadanie_spring/DemoZadanieSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.demo_zadanie_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZadanieSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
